d8520f5649fed5463f8c42bf8732ab80b8c9734c
tommysoeder <148865051+tommysoeder@users.noreply.github.com>
2023-11-23 11:36:48 +0100

    Add files via upload

---
+++ closures.js
@@ -0,0 +1,14 @@
+// Try to write your first closure.
+// Write a function called printName and declare inside it a variable called helloName, 
+// the value of this variable will be: "Hello John". Also declare a function named inner and return the helloName variable. 
+// The purpose is printing "Hello John" in the console.
+
+function printName(){
+    let helloName="Hello John";
+    function inner(){
+        return helloName;
+    }
+    return inner;
+}
+
+console.log(printName()());
