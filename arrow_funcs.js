cbe674eb269c6b06d86f70e22c1a80975c269076
tommysoeder <148865051+tommysoeder@users.noreply.github.com>
2023-11-23 10:14:45 +0100

    Add files via upload

---
+++ arrow_funcs.js
@@ -0,0 +1,33 @@
+// const sum = function (a, b) {
+//   return a + b;
+// }
+
+const sum = (a,b)=> (a+b);
+
+// const subtract = function (a, b) {
+//   return a - b;
+// }
+
+const subtract = (a,b) => (a-b);
+
+// const multiply = function (a, b) {
+//   return a * b;
+// }
+
+const multiply = (a,b) => (a*b);
+// const divide = function (a, b) {
+//   return a / b;
+// }
+
+const divide = (a,b) => (a/b);
+
+// const log = function (value) {
+//   console.log(value);
+// }
+
+const log = (value) => console.log(value);
+
+// ((2 + 4) * (5 + 2) - 2) / 5
+
+const result = divide(subtract(multiply(sum(2, 4), sum(5, 2)), 2), 5);
+
