ff126359c905f306d757cba4393ab1223590b791
tommysoeder <148865051+tommysoeder@users.noreply.github.com>
2023-11-30 12:56:49 +0100

    Add files via upload

---
+++ json2.js
@@ -0,0 +1,24 @@
+// Create the fromJson method that takes in a json as parameter and returns an object of type Person.
+
+class Person {
+    constructor(id, firstName, lastName, age) {
+      this.id = id;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+    }
+  
+    toJson() {
+      return JSON.stringify(this);
+    }
+  
+    static fromJson(json) {
+      const obj = JSON.parse(json);
+      return new Person(obj.id, obj.firstName, obj.lastName, obj.age);
+    }
+  }
+  
+  const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
+  const developer = Person.fromJson(json);
+  console.log(developer);
+  
