3270a6e6a4436155ef99c744502d65464ca0d398
tommysoeder <148865051+tommysoeder@users.noreply.github.com>
2023-12-10 11:59:23 +0100

    Add files via upload

---
+++ template_strings.js
@@ -0,0 +1,10 @@
+// Perform a code refactoring by using the template literals.
+
+class Person { 
+    constructor(firstName, lastName) {
+         this.firstName = firstName; 
+         this.lastName = lastName; }
+         }
+
+const developer = new Person("Mario", "Rossi"); 
+console.log (`${developer.firstName} ${developer.lastName}`); 
