20d146a8d2a43fa5592ac25c510078f213dbe2db
tommysoeder <148865051+tommysoeder@users.noreply.github.com>
2023-12-10 11:23:53 +0100

    Add files via upload

---
+++ instanceof.js
@@ -0,0 +1,43 @@
+// Define a class called AreaCalculator. 
+// Inside it, create a static method that let you calculate the area of the geometric figure passed as parameter.
+//  The method will let you calculate the square, the rectangle and the circle area.
+
+class Square {
+    constructor(side) {
+      this.side = side;
+    }
+  }
+  
+  class Rectangle {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+  
+  class Circle {
+    constructor(radius) {
+      this.radius = radius;
+    }
+  }
+  
+  class AreaCalculator {
+    static calculate(figure) {
+      if (figure instanceof Square) {
+        return figure.side * figure.side; 
+      } else if (figure instanceof Rectangle) {
+        return figure.width * figure.height; 
+      } else if (figure instanceof Circle) {
+        return Math.PI * figure.radius * figure.radius; 
+      }
+    }
+  }
+  
+  const square = new Square(4);
+  const rectangle = new Rectangle(4, 2);
+  const circle = new Circle(5);
+  
+  console.log(AreaCalculator.calculate(square));
+  console.log(AreaCalculator.calculate(rectangle));
+  console.log(AreaCalculator.calculate(circle));
+  
