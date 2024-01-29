a6c71f6cb2f3b29fcf5eac9491b11949c67c4eba
tommysoeder <148865051+tommysoeder@users.noreply.github.com>
2024-01-29 11:19:17 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import Forms from "./Components/Forms"
+import { HelloWorld } from "./HelloWorld"
+
+
+export function App() {
+    return (
+        <div>
+            <h1>My awesome App</h1>
+            <hr></hr>
+            <HelloWorld />
+            <Forms />            
+        </div>
+    )
+}
