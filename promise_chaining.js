531f837ff29e1aa33057fd5ec0a94d51fc692775
tommysoeder <148865051+tommysoeder@users.noreply.github.com>
2024-04-05 07:04:53 +0200

    Update promise_chaining.js

--- promise_chaining.js
+++ promise_chaining.js
@@ -1,24 +1,31 @@
-
 const isLogged = true;
-const firstPromise = (isLogged) => new Promise((resolve,reject)=>{
-    if(isLogged){
-        const rand=Math.random();
-        resolve(rand)
-    }else{
-        reject(new Error("unidentified"))
-    }
-})
 
-const secondPromise=(rand) => new Promise((resolve,reject)=>{
-    if(rand > 0.5){
-        const datos={name: "John", age: 24}
-        resolve(datos)
-    }else{
-        reject(new Error ("Número pequeño"))
-    }
-})
+const firstPromise = (isLogged) => new Promise((resolve, reject) => {
+  if (isLogged) {
+    const rand = Math.random();
+    resolve(rand);
+  } else {
+    reject(new Error("unidentified"));
+  }
+});
+
+const secondPromise = (rand) => new Promise((resolve, reject) => {
+  if (rand > 0.5) {
+    const datos = { name: "John", age: 24 };
+    resolve(datos);
+  } else {
+    reject(new Error("Número pequeño"));
+  }
+});
 
 firstPromise(isLogged)
- .then((rand)=> secondPromise(rand))
- .then((datos)=>console.log(datos))
- .catch((error) => console.log("ERROR", error))
+  .then((rand) => secondPromise(rand))
+  .then((datos) => {
+    console.log("Final Object:", datos);
+  })
+  .catch((error) => {
+    console.error("Error:", error.message);
+  })
+  .finally(() => {
+    console.log("Promise chain completed.");
+  });
